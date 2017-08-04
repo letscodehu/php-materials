6d5c297ce10407550ad0ec30937953fc3ebac86e
Papp Krisztián <fejlesztes@letscode.hu>
2017-08-04 18:09:59 +0200

    Design patterns #4

--- php-design-patterns/index.php
+++ php-design-patterns/index.php
@@ -8,19 +8,32 @@ interface LoggerInterface {
 }
 
 class MonologLogger implements LoggerInterface {
-
-    public function info($message) {
+    function info($message) {
         echo '[INFO] '. $message.PHP_EOL;
     }
-    public function warn($message) {
+    function warn($message) {
         echo '[WARN] '. $message.PHP_EOL;
     }
-    public function error($message) {
+    function error($message) {
         echo '[ERROR] '. $message.PHP_EOL;
     }
-    public function debug($message) {
+    function debug($message) {
         echo '[DEBUG] '. $message.PHP_EOL;
     }
+}
+
+class CheckoutController {
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    public function checkout($userId) {
+        $this->logger->info('User '. $userId. "# visited checkout page.");
+    }
+
 }
 
 interface Log4PHPInterface {
@@ -33,43 +46,28 @@ class ApacheLogger implements Log4PHPInterface {
     }
 }
 
-class CheckoutController {
-
-    private $logger;
-
-    public function __construct(LoggerInterface $logger) {
-        $this->logger = $logger;
-    }
-
-    public function checkout($userid) {
-        $this->logger->info('User '. $userid. "# visited checkout page");
-    }
-
-}
-
 class MonologLoggerAdapter implements LoggerInterface {
 
     private $logger;
 
-    public function __construct(\Log4PHPInterface $logger) {
+    public function __construct(Log4PHPInterface $logger) {
         $this->logger = $logger;
     }
 
-    public function info($message) {
+    function info($message) {
         $this->logger->log('INFO', $message);
     }
-
-    public function warn($message) {
+    function warn($message) {
         $this->logger->log('WARN', $message);
     }
-    public function error($message) {
-        $this->logger->log('ERROR',$message);
+    function error($message) {
+        $this->logger->log('ERROR', $message);
     }
-    public function debug($message) {
-        $this->logger->log('DEBUG',$message);
+    function debug($message) {
+        $this->logger->log('DEBUG', $message);
     }
-
 }
 
-$controller = new CheckoutController(new MonologLogger());
+$controller = new CheckoutController(new MonologLoggerAdapter(new ApacheLogger()));
+
 $controller->checkout(5);
