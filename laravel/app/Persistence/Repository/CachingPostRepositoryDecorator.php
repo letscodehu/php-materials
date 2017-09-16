1f1bbf54171c30c2049af92546f9aa60b9ce786f
Papp Krisztián <fejlesztes@letscode.hu>
2017-09-16 15:52:00 +0200

    Laravel blog #13 done

--- laravel/app/Persistence/Repository/CachingPostRepositoryDecorator.php
+++ laravel/app/Persistence/Repository/CachingPostRepositoryDecorator.php
@@ -8,6 +8,9 @@ use Illuminate\Cache\Repository;
 class CachingPostRepositoryDecorator extends AbstractPostRepositoryDecorator {
 
     const ALL_PUBLIC_POST = "allPublicPost";
+    const PAGE_KEY_TEMPLATE = "page_%s:%s";
+    const DEFAULT_CACHE_TTL = 60;
+    const MOST_VIEWED = "mostViewed";
 
     /**
      * @var PostRepository
@@ -31,11 +34,48 @@ class CachingPostRepositoryDecorator extends AbstractPostRepositoryDecorator {
     public function findAllPublic($page, $size)
     {
         $taggedCache = $this->cacheRepository->tags(self::ALL_PUBLIC_POST);
-        $key = sprintf("page_%s:%s", $page, $size);
-        $cachedValue = $taggedCache->get($key);
+        $key = sprintf(self::PAGE_KEY_TEMPLATE, $page, $size);
+        return $this->cacheFunctionResult($taggedCache, $key, self::DEFAULT_CACHE_TTL, function()
+        use ($page, $size) {
+            return $this->postRepository->findAllPublic($page, $size);
+        });
+    }
+
+    public function findMostViewed($limit)
+    {
+        $taggedCache = $this->cacheRepository->tags(self::MOST_VIEWED);
+        $key = sprintf("post:%s", $limit);
+        return $this->cacheFunctionResult($taggedCache, $key, self::DEFAULT_CACHE_TTL, function()
+            use ($limit) {
+            return $this->postRepository->findMostViewed($limit);
+        });
+    }
+
+    public function findByCategory($slug, $page, $size)
+    {
+        $taggedCache = $this->cacheRepository->tags("category");
+        $key = sprintf("%s:%s:%s", $slug, $page, $size);
+        return $this->cacheFunctionResult($taggedCache, $key, self::DEFAULT_CACHE_TTL, function()
+            use ($slug, $page, $size) {
+            return $this->postRepository->findByCategory($slug, $page, $size);
+        });
+    }
+
+    public function findByTag($slug, $page, $size)
+    {
+        $taggedCache = $this->cacheRepository->tags("tag");
+        $key = sprintf("%s:%s:%s", $slug, $page, $size);
+        return $this->cacheFunctionResult($taggedCache, $key, self::DEFAULT_CACHE_TTL, function()
+        use ($slug, $page, $size) {
+            return $this->postRepository->findByTag($slug, $page, $size);
+        });
+    }
+
+    private function cacheFunctionResult(Repository $storage, $cacheKey, $ttl, callable $callable) {
+        $cachedValue = $storage->get($cacheKey);
         if (is_null($cachedValue)) {
-            $cachedValue = $this->postRepository->findAllPublic($page, $size);
-            $taggedCache->put($key, $cachedValue);
+            $cachedValue = $callable();
+            $storage->put($cacheKey, $cachedValue, $ttl);
         }
         return $cachedValue;
     }
--- laravel/tests/Unit/CachingPostRepositoryDecoratorTest.php
+++ laravel/tests/Unit/CachingPostRepositoryDecoratorTest.php
@@ -11,6 +11,11 @@ use Tests\TestCase;
 
 class CachingPostRepositoryDecoratorTest extends TestCase {
 
+
+    /**
+     * @var TaggedCache
+     */
+    private $taggedCache;
     /**
      * @var PostRepository
      */
@@ -31,6 +36,9 @@ class CachingPostRepositoryDecoratorTest extends TestCase {
         $this->mockCache = $this->getMockBuilder(Repository::class)
             ->disableOriginalConstructor()
             ->getMock();
+        $this->taggedCache = $this->getMockBuilder(TaggedCache::class)
+            ->disableOriginalConstructor()
+            ->getMock();
         $this->underTest = new CachingPostRepositoryDecorator($this->mockRepository, $this->mockCache);
     }
 
@@ -43,19 +51,15 @@ class CachingPostRepositoryDecoratorTest extends TestCase {
         $expectedResult = "cachedValue";
         $key = "page_1:20";
         $tagname = "allPublicPost";
-
-        $taggedCache = $this->getMockBuilder(TaggedCache::class)
-            ->disableOriginalConstructor()
-            ->getMock();
         $this->mockCache->expects($this->once())
             ->method("tags")
             ->with($tagname)
-            ->willReturn($taggedCache);
-        $taggedCache
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
             ->expects($this->never())
             ->method("has")
             ->with($key);
-        $taggedCache
+        $this->taggedCache
             ->expects($this->once())
             ->method("get")
             ->with($key)
@@ -75,14 +79,11 @@ class CachingPostRepositoryDecoratorTest extends TestCase {
         $expectedResult = "fromDB";
         $key = "page_1:20";
         $tagname = "allPublicPost";
-        $taggedCache = $this->getMockBuilder(TaggedCache::class)
-            ->disableOriginalConstructor()
-            ->getMock();
         $this->mockCache->expects($this->once())
             ->method("tags")
             ->with($tagname)
-            ->willReturn($taggedCache);
-        $taggedCache
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
             ->expects($this->once())
             ->method("get")
             ->with($key)
@@ -91,12 +92,192 @@ class CachingPostRepositoryDecoratorTest extends TestCase {
             ->method("findAllPublic")
             ->with(1,20)
             ->willReturn($expectedResult);
-        $taggedCache
+        $this->taggedCache
             ->expects($this->once())
             ->method("put")
-            ->with($key, $expectedResult);
+            ->with($key, $expectedResult, 60);
         // WHEN
         $actual = $this->underTest->findAllPublic(1,20);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findMostViewed_should_return_cached_value()
+    {
+        // GIVEN
+        $expectedResult = "cachedValue";
+        $key = "post:20";
+        $tagName = "mostViewed";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagName)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn($expectedResult);
+        // WHEN
+        $actual = $this->underTest->findMostViewed(20);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findMostViewed_should_store_in_cache()
+    {
+        // GIVEN
+        $expectedResult = "fromDB";
+        $key = "post:20";
+        $tagname = "mostViewed";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagname)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn(null);
+        $this->mockRepository->expects($this->once())
+            ->method("findMostViewed")
+            ->with(20)
+            ->willReturn($expectedResult);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("put")
+            ->with($key, $expectedResult, 60);
+        // WHEN
+        $actual = $this->underTest->findMostViewed(20);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findByCategory_should_return_cached_value()
+    {
+        // GIVEN
+        $expectedResult = "cachedValue";
+        $category = "Ezekiel";
+        $page = 25;
+        $size = 17;
+        $key = "Ezekiel:25:17";
+        $tagName = "category";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagName)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn($expectedResult);
+        // WHEN
+        $actual = $this->underTest->findByCategory($category, $page, $size);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findByCategory_should_store_value_in_cache()
+    {
+        // GIVEN
+        $expectedResult = "fromDB";
+        $category = "Ezekiel";
+        $page = 25;
+        $size = 17;
+        $key = "Ezekiel:25:17";
+        $tagName = "category";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagName)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn(null);
+        $this->mockRepository->expects($this->once())
+            ->method("findByCategory")
+            ->with($category, $page, $size)
+            ->willReturn($expectedResult);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("put")
+            ->with($key, $expectedResult, 60);
+        // WHEN
+        $actual = $this->underTest->findByCategory($category, $page, $size);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findByTag_should_return_cached_results()
+    {
+        // GIVEN
+        $expectedResult = "cachedValue";
+        $tag = "Ezekiel";
+        $page = 25;
+        $size = 17;
+        $key = "Ezekiel:25:17";
+        $tagName = "tag";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagName)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn($expectedResult);
+        // WHEN
+        $actual = $this->underTest->findByTag($tag, $page, $size);
+        // THEN
+        $this->assertEquals($expectedResult, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function findByTag_should_fill_cache_with_values()
+    {
+        // GIVEN
+        $expectedResult = "fromDB";
+        $slug = "Ezekiel";
+        $page = 25;
+        $size = 17;
+        $key = "Ezekiel:25:17";
+        $tagName = "tag";
+        $this->mockCache->expects($this->once())
+            ->method("tags")
+            ->with($tagName)
+            ->willReturn($this->taggedCache);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("get")
+            ->with($key)
+            ->willReturn(null);
+        $this->mockRepository->expects($this->once())
+            ->method("findByTag")
+            ->with($slug, $page, $size)
+            ->willReturn($expectedResult);
+        $this->taggedCache
+            ->expects($this->once())
+            ->method("put")
+            ->with($key, $expectedResult, 60);
+        // WHEN
+        $actual = $this->underTest->findByTag($slug, $page, $size);
         // THEN
         $this->assertEquals($expectedResult, $actual);
     }
