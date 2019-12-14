99fdc77c8aeed0703f3ccd8cc7fe5408d9f362e3
Krisztian Papp <fejlesztes@letscode.hu>
2019-12-14 13:34:31 +0100

    design patterns 21

--- php-design-patterns/lazy-loading/index.php
+++ php-design-patterns/lazy-loading/index.php
@@ -5,14 +5,86 @@ class User {
     private $type;
     private $name;
     private $email;
-    private $assets;
+    private $subscriptions;
 
-    public function __construct($type, $name, $email) {
+    /**
+     * User constructor.
+     * @param $type
+     * @param $name
+     * @param $email
+     */
+    public function __construct($type, $name, $email)
+    {
         $this->type = $type;
         $this->name = $name;
         $this->email = $email;
     }
 
-    public function __
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubscriptions()
+    {
+        if ($this->subscriptions == null) {
+            $this->subscriptions = getSubscriptions();
+        }
+        return $this->subscriptions;
+    }
 
 }
+
+function getSubscriptions() {
+    echo "Getting subscriptions...".PHP_EOL;
+    usleep(200000);
+    return [];
+}
+
+
+$start = microtime(true);
+
+$users = [
+    new User("community", "Someone", "some@email.com"),
+    new User("community", "Someone", "some@email.com"),
+    new User("community", "Someone", "some@email.com"),
+    new User("gold", "Someone", "some@email.com"),
+    new User("silver", "Someone", "some@email.com")
+];
+
+// viewmodel
+$viewModelUsers = [];
+foreach ($users as $user) {
+    $viewModelUser = [];
+    if ($user->getType() != 'community') {
+        $viewModelUser["subscriptions"] = $user->getSubscriptions();
+    } else {
+        $viewModelUser["subscriptions"] = [];
+    }
+    $viewModelUsers[] = $viewModelUser;
+}
+
+$stop = microtime(true);
+echo ($stop-$start). " sec".PHP_EOL;
--- php-design-patterns/table-data-gateway/index.php
+++ php-design-patterns/table-data-gateway/index.php
@@ -1,1 +1,69 @@
 <?php
+
+class OrderTableDataGateway {
+
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    /**
+     * OrderTableDataGateway constructor.
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function find($id) {
+        $statement = $this->pdo->prepare("SELECT id, total, billing_city, billing_name, billing_address FROM orders WHERE id = :id");
+        $statement->execute([
+            "id" => $id
+        ]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function save(array $order) {
+        if (array_key_exists("id", $order) && $this->find($order["id"])) {
+            return $this->update($order);
+        } else {
+            return $this->create($order);
+        }
+    }
+
+    private function update(array $order)
+    {
+        $statement = $this->pdo->prepare("UPDATE orders SET total = :total, billing_city = :billing_city,
+            billing_name = :billing_name, billing_address = :billing_address WHERE id = :id");
+        $statement->execute($order);
+        return $order;
+    }
+
+    private function create(array $order)
+    {
+        $order["id"] = uniqid(true);
+        $statement = $this->pdo->prepare("INSERT INTO orders (id, total, billing_city, billing_name, billing_address) 
+                VALUES (:id, :total, :billing_city, :billing_name, :billing_address)");
+        $statement->execute($order);
+        return $order;
+    }
+
+}
+
+$pdo = new PDO("mysql:hostname=localhost;dbname=training", "training", "password");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$orderGateway = new OrderTableDataGateway($pdo);
+
+$order = [
+    "total" => 500,
+    "billing_address" => "address",
+    "billing_city" => "city",
+    "billing_name" => "name"
+];
+
+$saved = $orderGateway->save($order);
+
+$order = $orderGateway->find($saved["id"]);
+var_dump($order);
